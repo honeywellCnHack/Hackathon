c0b629240388848162a8ad86a3193f264fef6e95
Ou, Jibin <Jibin.Ou@Honeywell.com>
2017-03-06 15:30:41 +0800

    Added fake location

--- AirQuality/app/src/main/java/com/test/poweroptimizer/MapFragment.java
+++ AirQuality/app/src/main/java/com/test/poweroptimizer/MapFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.app.FragmentManager;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -57,6 +58,13 @@ public class MapFragment extends Fragment  implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.map = googleMap;
-        this.map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("FirstPage"));
+        this.map.addMarker(new MarkerOptions().position(new LatLng(31.235041, 121.509421)).title("Good"));
+        this.map.addMarker(new MarkerOptions().position(new LatLng(31.233836, 121.513384)).title("Good"));
+        this.map.addMarker(new MarkerOptions().position(new LatLng(31.232044, 121.518591)).title("Good"));
+        MarkerOptions bad = new MarkerOptions().position(new LatLng(31.230117, 121.524717)).title("Bad");
+        this.map.addMarker(bad);
+        this.map.addMarker(new MarkerOptions().position(new LatLng(31.232906, 121.523526)).title("Good"));
+        LatLng chLocation = new LatLng(31.232044, 121.518591);
+        this.map.animateCamera(CameraUpdateFactory.newLatLngZoom(chLocation,14));
     }
 }
